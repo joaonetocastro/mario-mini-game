c69c4e8480f8bb9c4e73cfdd742aade3ba1ca11e
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-22 10:03:29 -0300

    updating docs

---
+++ docs/precache-manifest.0283beeee09457ad791f1c1b84c38d27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e06280575d066f23780e946ef505618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de4e31826184ab783575",
+    "url": "/static/css/main.3d1fc973.chunk.css"
+  },
+  {
+    "revision": "421508f298c2acc0a05e",
+    "url": "/static/js/2.95bf0643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95bf0643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4e31826184ab783575",
+    "url": "/static/js/main.e29d8e36.chunk.js"
+  },
+  {
+    "revision": "5fa2e55d4036e9a53330",
+    "url": "/static/js/runtime-main.83ffa53a.js"
+  },
+  {
+    "revision": "11d040ec451f0dabe354faa39c94a4e3",
+    "url": "/static/media/mario.11d040ec.png"
+  }
+]);
