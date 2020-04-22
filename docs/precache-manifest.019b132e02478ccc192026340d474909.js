142b0132744f6f11bd12d4c1c04230cec3f4c734
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-22 09:44:18 -0300

    pushing /docs
    deleting docs
    
    updating docs
    
    updating public path
    
    deleting docs
    
    pushing docs

---
+++ docs/precache-manifest.019b132e02478ccc192026340d474909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a55f4839efd7b3f6552e3a7461c45e2",
+    "url": "/mario-mini-game/index.html"
+  },
+  {
+    "revision": "e2dfcc5c6a401f1fcb40",
+    "url": "/mario-mini-game/static/css/main.3d1fc973.chunk.css"
+  },
+  {
+    "revision": "421508f298c2acc0a05e",
+    "url": "/mario-mini-game/static/js/2.95bf0643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mario-mini-game/static/js/2.95bf0643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2dfcc5c6a401f1fcb40",
+    "url": "/mario-mini-game/static/js/main.e79457e2.chunk.js"
+  },
+  {
+    "revision": "de3f9c67506534b009cf",
+    "url": "/mario-mini-game/static/js/runtime-main.37bd3b50.js"
+  },
+  {
+    "revision": "11d040ec451f0dabe354faa39c94a4e3",
+    "url": "/mario-mini-game/static/media/mario.11d040ec.png"
+  }
+]);
